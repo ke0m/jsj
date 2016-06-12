365e1ab490a13fff787661012c25308b45e2c97f
Joseph Jennings <joseph.s.jennings@gmail.com>
2016-06-12 11:40:15 -0700

    First commit

---
+++ src/weq/AcstcWfldFD.java
@@ -0,0 +1,34 @@
+package weq;
+
+import edu.mines.jtk.awt.*;
+import edu.mines.jtk.dsp.*;
+import edu.mines.jtk.mosaic.*;
+import edu.mines.jtk.util.*;
+import static edu.mines.jtk.util.ArrayMath.*;
+
+/* Finite difference acoustic wave equation
+ * solver for constant density.
+ * 
+ * @author Joseph Jennings, Stanford University
+ * @acknowledgements Guillaume Barnier; Ali Almomin, Stanford University
+ *                   Simon Luo; Dave Hale, Colorado School of Mines
+ * @version 2016.06.11 
+ */
+
+public class AcstcWfldFD {
+	
+	//TODO: Add sampling in x,z and t
+	//      but I need to remember the CFL condition
+	//      and write a method for that
+	public AcstcWfldFD(Sampling x, Sampling z, Sampling t) {
+	}
+	
+	public void forwardProp() {
+		
+	}
+	
+	public void adjointProp() {
+		
+	}
+
+}
---
+++ src/weq/WaveEqOps.java
@@ -0,0 +1,11 @@
+package weq;
+
+import edu.mines.jtk.awt.*;
+import edu.mines.jtk.dsp.*;
+import edu.mines.jtk.mosaic.*;
+import edu.mines.jtk.util.*;
+import static edu.mines.jtk.util.ArrayMath.*;
+
+public class WaveEqOps {
+
+}
