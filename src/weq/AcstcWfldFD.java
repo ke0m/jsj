4dcec188dd12552c3f7d5642609383eea31f060f
Joseph Jennings <joseph.s.jennings@gmail.com>
2016-06-13 22:17:12 -0700

    Added constructor and methods for checking stability and dispersion

--- src/weq/AcstcWfldFD.java
+++ src/weq/AcstcWfldFD.java
@@ -18,10 +18,16 @@ import static edu.mines.jtk.util.ArrayMath.*;
 
 public class AcstcWfldFD {
 	
-	//TODO: Add sampling in x,z and t
-	//      but I need to remember the CFL condition
-	//      and write a method for that
-	public AcstcWfldFD(Sampling x, Sampling z, Sampling t) {
+	/**
+	 * Constructs an acoustic wavefield object for propagating acoustic wavefields
+	 * @param sx sampling of grid in x
+	 * @param sz sampling of grid in z
+	 * @param st sampling of grid in t
+	 */
+	public AcstcWfldFD(Sampling sx, Sampling sz, Sampling st) {
+		_nx = sx.getCount(); _nz = sz.getCount(); _nt = st.getCount();
+		_dx = sx.getDelta(); _dz = sz.getDelta(); _dt = st.getDelta();
+		_fx = sx.getFirst(); _fz = sz.getFirst(); _ft = st.getFirst();
 	}
 	
 	public void forwardProp() {
@@ -32,22 +38,74 @@ public class AcstcWfldFD {
 		
 	}
 	
-
-	
-	public boolean checkCFL(Sampling x, Sampling z, Sampling t, float[][] v) {
+	/**
+	 * Checks the stability of the 2D acoustic propagator based on the 
+	 * Courant-Fleury-Lewis (CFL) condition
+	 * @param v input velocity
+	 * @return a boolean indicating if the CFL condition is satisfied
+	 */
+	public boolean checkCFL(float[][] v) {
 		
-		boolean test = false;
+		float vmax     = max(v);
+		boolean stable = false;
+		float chk    = 0.f;
 		
-		return test;
+		if(_dx > _dz) {
+			chk = (float)(_dt*vmax/_dz);
+		}
+		else if(_dx < _dz) {
+			chk = (float)(_dt*vmax/_dx);
+		}
+		else {
+			chk = (float)(_dt*vmax/_dx);
+		}
+		
+		if(chk < CFL) {
+			stable = true;
+		}
+		
+		return stable;
 	}
 	
-	public boolean checkPtsPerLength(Sampling x, Sampling z, float[][] v, float[] src) {
-		boolean test = false;
+	/**
+	 * Checks for dispersive wave propagation for 2D 
+	 * second order in time and fourth order in space
+	 * derivatives
+	 * @param v input velocity
+	 * @return if the condition is satisfied, returns the 
+	 *         number of gridpoints per wavelength. Else,
+	 *         returns 0.
+	 */
+	public float checkPtsPerLength(float[][] v) {
 		
-		return test;
+		float vmin   = min(v);		
+		double fmax  = 1/(2*_dt);
+		float ppl    = 0.f;
+		
+		if(_dx > _dz) {
+			ppl = (float)(vmin/fmax*_dx);
+		}
+		else if(_dx < _dz){
+			ppl = (float)(vmin/fmax*_dz);
+		}
+		else {
+			ppl = (float)(vmin/fmax*_dx);
+		}
+		
+		if(ppl < T2S4) {
+			ppl = 0.f;
+		}
+		
+		return ppl;
 	}
 	
 	///////////////////////////////////////////////////////////////////////
 	// private
+	private int _nx, _nz, _nt;
+	private double _dx, _dz, _dt;
+	private double _fx, _fz, _ft;
+	
+	private static final double CFL = sqrt(2)/2;
+	private static final int T2S4 = 4;
 	
 }
