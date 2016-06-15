cdd2ad84edc29ae85bc6eb9dbe48c3b52a113542
Joseph Jennings <joseph.s.jennings@gmail.com>
2016-06-14 22:40:05 -0700

    Constructor now contains checks for dispersion and stability

--- src/weq/AcstcWfldFD.java
+++ src/weq/AcstcWfldFD.java
@@ -20,23 +20,41 @@ public class AcstcWfldFD {
 	
 	/**
 	 * Constructs an acoustic wavefield object for propagating acoustic wavefields
+	 * and checks for stability and dispersion in propagation.
 	 * @param sx sampling of grid in x
 	 * @param sz sampling of grid in z
-	 * @param st sampling of grid in t
+	 * @param st sampling of wavefield in t
 	 */
-	public AcstcWfldFD(Sampling sx, Sampling sz, Sampling st) {
+	public AcstcWfldFD(Sampling sx, Sampling sz, Sampling st, float[][] v, float fmax) {
 		_nx = sx.getCount(); _nz = sz.getCount(); _nt = st.getCount();
 		_dx = sx.getDelta(); _dz = sz.getDelta(); _dt = st.getDelta();
 		_fx = sx.getFirst(); _fz = sz.getFirst(); _ft = st.getFirst();
+		
+		Check.state(checkCFL(v), "Error: please change your samplings. Your propagator will be unstable");
+
+		if(checkPtsPerLength(v,fmax) == 0.f) {
+			boolean disp = false;
+			Check.state(disp, "Error: please change your samplings. Your propagation will be dispersive.");
+		}
 	}
 	
-	public void forwardProp() {
+	public void forward(float[][][] src, float[][][] wfld) {
+		//TODO: First need a stencil
 		
 	}
 	
-	public void adjointProp() {
-		
+	public void adjoint(float[][][] wfld, float[][][] src) {
 	}
+	
+	
+	///////////////////////////////////////////////////////////////////////
+	// private
+	private int _nx, _nz, _nt;
+	private double _dx, _dz, _dt;
+	private double _fx, _fz, _ft;
+	
+	private static final double CFL = sqrt(2)/2;
+	private static final int T2S4 = 4;
 	
 	/**
 	 * Checks the stability of the 2D acoustic propagator based on the 
@@ -44,7 +62,7 @@ public class AcstcWfldFD {
 	 * @param v input velocity
 	 * @return a boolean indicating if the CFL condition is satisfied
 	 */
-	public boolean checkCFL(float[][] v) {
+	private boolean checkCFL(float[][] v) {
 		
 		float vmax     = max(v);
 		boolean stable = false;
@@ -72,23 +90,21 @@ public class AcstcWfldFD {
 	 * second order in time and fourth order in space
 	 * derivatives
 	 * @param v input velocity
-	 * @return if the condition is satisfied, returns the 
+	 * @param fmax the maximum frequency of the source wavelet
+	 * @return if sufficient enough gridpoints per wavelength, returns the 
 	 *         number of gridpoints per wavelength. Else,
 	 *         returns 0.
 	 */
-	public float checkPtsPerLength(float[][] v) {
+	public float checkPtsPerLength(float[][] v, float fmax) {
 		
-		float vmin   = min(v);		
-		double fmax  = 1/(2*_dt);
+		float vmin   = min(v);;
 		float ppl    = 0.f;
 		
 		if(_dx > _dz) {
 			ppl = (float)(vmin/fmax*_dx);
-		}
-		else if(_dx < _dz){
+		} else if(_dx < _dz){
 			ppl = (float)(vmin/fmax*_dz);
-		}
-		else {
+		} else {
 			ppl = (float)(vmin/fmax*_dx);
 		}
 		
@@ -98,14 +114,8 @@ public class AcstcWfldFD {
 		
 		return ppl;
 	}
-	
-	///////////////////////////////////////////////////////////////////////
-	// private
-	private int _nx, _nz, _nt;
-	private double _dx, _dz, _dt;
-	private double _fx, _fz, _ft;
-	
-	private static final double CFL = sqrt(2)/2;
-	private static final int T2S4 = 4;
+
+	//private float[][] applyStencil4() {
+	//}
 	
 }
