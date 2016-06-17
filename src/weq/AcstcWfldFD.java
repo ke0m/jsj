d48858d1dab985e8e09f6fc0080e27923eefd8ae
Joseph Jennings <joseph.s.jennings@gmail.com>
2016-06-16 19:38:52 -0700

    Separation of private and public

--- src/weq/AcstcWfldFD.java
+++ src/weq/AcstcWfldFD.java
@@ -1,8 +1,6 @@
 package weq;
 
-import edu.mines.jtk.awt.*;
 import edu.mines.jtk.dsp.*;
-import edu.mines.jtk.mosaic.*;
 import edu.mines.jtk.util.*;
 import static edu.mines.jtk.util.ArrayMath.*;
 
@@ -100,6 +98,9 @@ public class AcstcWfldFD {
 		
 		return ppl;
 	}
+	
+	///////////////////////////////////////////////////////////////////////
+	// private
 
 	//private float[][] applyStencil4() {
 	//}
