dd03207658246b8c32c0d4771877104ca63aa2eb
Joseph Jennings <joseph.s.jennings@gmail.com>
2016-06-28 08:22:22 -0700

    Changed formatting

--- src/weq/SourceScale.java
+++ src/weq/SourceScale.java
@@ -19,39 +19,39 @@ public class SourceScale {
 	 * @param dtwf the temporal sampling of the wavefield
 	 * @param vel the input velocity
 	 */
-	public SourceScale(double dtwf, float[][] vel) {
-		_dtwf = (float)dtwf;
-		_vel = vel;
+  public SourceScale(double dtwf, float[][] vel) {
+	  _dtwf = (float)dtwf;
+	  _vel = vel;
 	}
 	
 	public void forward(float[][][] psrc, float spsrc[][][]) {
-		int nx    = psrc[0][0].length;
-		int nz    = psrc[0].length;
-		int ntsrc = psrc.length;
-		for(int it = 0; it < ntsrc; ++it){
-			for(int iz = 0; iz < nz; ++iz){
-				for(int ix = 0; ix < nx; ++ix){
-					float v2  = _vel[iz][ix]*_vel[iz][ix];
-					float dt2 = _dtwf*_dtwf;
-					spsrc[it][iz][ix] += -v2*dt2*psrc[it][iz][ix];
-				}
-			}
-		}
+	  int nx    = psrc[0][0].length;
+	  int nz    = psrc[0].length;
+	  int ntsrc = psrc.length;
+	  for(int it = 0; it < ntsrc; ++it){
+	    for(int iz = 0; iz < nz; ++iz){
+	      for(int ix = 0; ix < nx; ++ix){
+	        float v2  = _vel[iz][ix]*_vel[iz][ix];
+	        float dt2 = _dtwf*_dtwf;
+	        spsrc[it][iz][ix] += -v2*dt2*psrc[it][iz][ix];
+	      }
+	    }
+	  }
 	}
 	
 	public void adjoint(float[][][] spsrc, float psrc[][][]) {
-		int nx    = psrc[0][0].length;
-		int nz    = psrc[0].length;
-		int ntsrc = psrc.length;
-		for(int it = 0; it < ntsrc; ++it){
-			for(int iz = 0; iz < nz; ++iz){
-				for(int ix = 0; ix < nx; ++ix){
-					float v2  = _vel[iz][ix]*_vel[iz][ix];
-					float dt2 = _dtwf*_dtwf;
-					psrc[it][iz][ix] += -v2*dt2*spsrc[it][iz][ix];
-				}
-			}
-		}
+	  int nx    = psrc[0][0].length;
+	  int nz    = psrc[0].length;
+	  int ntsrc = psrc.length;
+	  for(int it = 0; it < ntsrc; ++it){
+	    for(int iz = 0; iz < nz; ++iz){
+	      for(int ix = 0; ix < nx; ++ix){
+	        float v2  = _vel[iz][ix]*_vel[iz][ix];
+	        float dt2 = _dtwf*_dtwf;
+	        psrc[it][iz][ix] += -v2*dt2*spsrc[it][iz][ix];
+	      }
+	    }
+	  }
 	}
 	
 	/////////////////////////////////////////////////
