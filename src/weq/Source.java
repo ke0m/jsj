eb71f225d2a22e23f408440bd7ba2d9b090d4ae9
Joseph Jennings <joseph.s.jennings@gmail.com>
2016-06-28 08:37:12 -0700

    Removed unecessary packages

--- src/weq/Source.java
+++ src/weq/Source.java
@@ -1,9 +1,6 @@
 package weq;
 
-import edu.mines.jtk.awt.*;
 import edu.mines.jtk.dsp.*;
-import edu.mines.jtk.mosaic.*;
-import edu.mines.jtk.util.*;
 import static edu.mines.jtk.util.ArrayMath.*;
 
 /** 
