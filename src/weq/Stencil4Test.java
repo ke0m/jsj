1ad8c00337e5024b331c6a57ce23cad364270289
Joseph Jennings <joseph.s.jennings@gmail.com>
2016-07-08 19:50:46 -0700

    Public and private test. Both pass and nice plotting example

--- src/weq/Stencil4Test.java
+++ src/weq/Stencil4Test.java
@@ -6,9 +6,17 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import edu.mines.jtk.dsp.*;
+import edu.mines.jtk.mosaic.*;
 import static edu.mines.jtk.util.ArrayMath.*;
 
 import java.util.Random;
+
+/**
+ * Tests {@link weq.AcstcWfldFD}.
+ * @author Joseph Jennings
+ * @acknowledgement Dave Hale, Colorado School of Mines
+ * @version 2016.07.07
+ */
 
 public class Stencil4Test extends TestCase {
 
@@ -17,17 +25,55 @@ public class Stencil4Test extends TestCase {
     junit.textui.TestRunner.run(suite);
   }
   
-  public void testDp() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+  public void testDppb(){
     int nx = 100; int nz = 100; int nt = 100;
-    Sampling sx = new Sampling(nx,1.0,0.0);
-    Sampling sz = new Sampling(nz,1.0,0.0);
-    Sampling st = new Sampling(nt,0.5,1.0);
+    Sampling sx = new Sampling(nx+4,1.0,0.0);
+    Sampling sz = new Sampling(nz+4,1.0,0.0);
+    Sampling st = new Sampling(nt,0.1,1.0);
+    ZeroPad zp = new ZeroPad(2,2);
     Random r = new Random(1992);
     float f = 0.00001f;
+    float [][] mp  = zerofloat(nx+4,nz+4);
+    float [][] dp  = zerofloat(nx+4,nz+4);
+    float [][] m   = sub(randfloat(r,nx,nz),0.5f);
+    float [][] d   = sub(randfloat(r,nx,nz),0.5f);
+    float [][] ms  = zerofloat(nx+4,nz+4);
+    float [][] ds  = zerofloat(nx+4,nz+4);
+    float [][] mst = zerofloat(nx,nz);
+    float [][] dst = zerofloat(nx,nz);
+    zp.forward(m, mp);
+    zp.forward(d, dp);
+    SimplePlot sp = new SimplePlot();
+    PixelsView pv = sp.addPixels(dp);
+    pv.setInterpolation(PixelsView.Interpolation.NEAREST);
+    AcstcWfldFD prp = new AcstcWfldFD(sx,sz,st,abs(m),f);
+    prp.forward4Stencil(mp,ds);
+    prp.adjoint4Stencil(dp,ms);
+    zp.adjoint(ds, dst);
+    zp.adjoint(ms, mst);
+    float dotm = dot(m,mst);
+    float dotd = dot(d,dst);
+    assertEquals(dotm,dotd,0.0001f);
+  }
+  
+  public void testDppv() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+    int nx = 100; int nz = 100; int nt = 100;
+    Sampling sx = new Sampling(nx+4,1.0,0.0);
+    Sampling sz = new Sampling(nz+4,1.0,0.0);
+    Sampling st = new Sampling(nt,0.5,1.0);
+    Random r = new Random(1992);
+    ZeroPad zp = new ZeroPad(2,2);
+    float f = 0.00001f;
+    float [][] mp  = zerofloat(nx+4,nz+4);
+    float [][] dp  = zerofloat(nx+4,nz+4);
     float [][] m  = sub(randfloat(r,nx,nz),0.5f);
     float [][] d  = sub(randfloat(r,nx,nz),0.5f);
-    float [][] ms = zerofloat(nx,nz);
-    float [][] ds = zerofloat(nx,nz);
+    float [][] ms = zerofloat(nx+4,nz+4);
+    float [][] ds = zerofloat(nx+4,nz+4);
+    float [][] mst = zerofloat(nx,nz);
+    float [][] dst = zerofloat(nx,nz);
+    zp.forward(m, mp);
+    zp.forward(d, dp);
     AcstcWfldFD prp = new AcstcWfldFD(sx,sz,st,abs(m),f);
     Class[] parameterTypes = new Class[2];
     Class[] ptype = new Class[1];
@@ -46,16 +92,18 @@ public class Stencil4Test extends TestCase {
     }
     mf.setAccessible(true); ma.setAccessible(true);
     Object[] parametersf = new Object[2];
-    parametersf[0] = m;
+    parametersf[0] = mp;
     parametersf[1] = ds;
     mf.invoke(prp, parametersf);
     Object[] parametersa = new Object[2];
-    parametersa[0] = d;
+    parametersa[0] = dp;
     parametersa[1] = ms;
     ma.invoke(prp, parametersa);
-    float dotm = dot(m,ms);
-    float dotd = dot(d,ds);
-    assertEquals(dotm,dotd,0.00001f);
+    zp.adjoint(ds, dst);
+    zp.adjoint(ms, mst);
+    float dotm = dot(m,mst);
+    float dotd = dot(d,dst);
+    assertEquals(dotm,dotd,0.0001f);
   }
   private static float dot(float[][] x, float[][] y) {
     return sum(mul(x,y));
